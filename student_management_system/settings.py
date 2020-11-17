46ca79be875b7dd45313f2609758380d031824d3
dagane <ahmed>
2020-11-17 15:32:49 +0300

    Updated database in settings.py

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -154,3 +153,5 @@ EMAIL_FILE_PATH=os.path.join(BASE_DIR,"sent_mails")
 #import dj_database_url
 #prod_db=dj_database_url.config(conn_max_age=500)
 #DATABASES['default'].update(prod_db)
+
+django_heroku.settings(locals())
